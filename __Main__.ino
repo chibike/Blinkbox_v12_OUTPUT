9fee7c1af82906e6151856925a90f09e776c50a1
Chibuike Okpaluba <chibike@users.noreply.github.com>
2016-06-12 04:19:18 +0100

    Fixed Bug in __main__

--- __Main__.ino
+++ __Main__.ino
@@ -6,7 +6,7 @@ void setup()
   Serial.begin(9600);
   Blink_OS_v12.begin();
   //Blink_OS_v12.fd_debug(120, 100);
-  Blink_OS_v12.pheripherals.CompassSensor.setTargetHeading( Blink_OS_v12pheripherals.CompassSensor.getHeading() );
+  Blink_OS_v12.pheripherals.CompassSensor.setTargetHeading( Blink_OS_v12.pheripherals.CompassSensor.getHeading() );
 }
 
 void loop()
